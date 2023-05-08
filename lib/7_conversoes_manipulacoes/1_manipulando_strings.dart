59aad0f6a97298853708c8f2f5eebb25d5eb35bc
Alessandro Pino <alessandro@profissionalti.com>
2023-05-07 22:22:32 -0300

    conversoes e manipulacoes - manipulando strings

--- lib/7_conversoes_manipulacoes/1_manipulando_strings.dart
+++ lib/7_conversoes_manipulacoes/1_manipulando_strings.dart
@@ -29,4 +29,70 @@ void main() {
   if (nome.contains('Pino')) {
     print('É da família Pino');
   }
+
+  // Interpolação;
+
+  var primeiroNome = 'Alessandro';
+  var segundoNome = 'Pino';
+
+  // saudacao criada com CONCATENAÇÃO
+  var saudacao =
+      'Olá ' + primeiroNome + ' ' + segundoNome + ', seja muito bem vindo';
+  print(saudacao);
+
+  // saudacao criada com INTERPOLAÇÃO
+  var saudacao2 = 'Olá $primeiroNome $segundoNome, seja muito bem vindo';
+  print(saudacao2);
+
+  // Utilizar {} depois de $ quando há uma chamada de método, um bloco de código a ser executado
+  print('Olá ${primeiroNome.toLowerCase()}');
+
+  var paciente = 'Alessandro Pino|37|Dev Dart e Flutter|RN';
+
+  // Para separar com 'substring', tem que saber o tamanho exato das strings para separar
+  var nomePaciente = paciente.substring(0, 15);
+  var idadePaciente = paciente.substring(16, 18);
+  print(nomePaciente);
+  print(idadePaciente);
+
+  // Ao utilizar 'split', pode-se definir o caractere separador
+  var dadosPaciente = paciente.split('|');
+  print(dadosPaciente);
+  print(dadosPaciente[0]);
+  print(dadosPaciente[1]);
+  print(dadosPaciente[2]);
+  print(dadosPaciente[3]);
+
+  // For in
+  for (var dado in dadosPaciente) {
+    print(dado);
+  }
+
+  // For each - forma 1
+  dadosPaciente.forEach((dado) => print(dado));
+
+  // For each - forma 2
+  dadosPaciente.forEach(print);
+
+  var pacientes = [
+    'Alessandro Pino|37|Dev Dart e Flutter|RN',
+    'Ninha Pino|25|Designer|RN'
+  ];
+
+  // Para "varrer" a lista acima, pode-se utilizar For each ou For in
+  for (paciente in pacientes) {
+    var dadosPaciente = paciente.split('|');
+    print(dadosPaciente[0]);
+  }
+
+  // Pegar apenas o sobrenome de cada um deles
+  for (paciente in pacientes) {
+    var dadosPaciente = paciente.split('|');
+    print(dadosPaciente[0].split(' ').last);
+  }
+
+  // Para imprimir o '$' ao invés de considerar variável, utilizar '\$'
+  var preco = 5.90;
+  var valor = 'O valor é R\$${preco.toStringAsFixed(2)}';
+  print(valor);
 }
